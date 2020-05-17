b8e0013ca32f69bf798b2f7733549f3a8e558c15
LittleSogo <847034331@qq.com>
2020-05-17 15:59:13 +0800

    Initial commit

---
+++ src/test/java/com/szcrm/SzcrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzcrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
